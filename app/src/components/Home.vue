f85166536d29886516808a9b3bcaa0fdafd8a7cc
captfurball <elys.1993a@gmail.com>
2020-09-08 00:40:44 +0800

    Added comments and likes

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -67,14 +67,70 @@
                                 align="center"
                                 justify="end">
 
-                            <v-icon class="mr-1">mdi-heart</v-icon>
-                            <span class="subheading mr-2">{{ story.likes.length }}</span>
-                            <span class="mr-1">·</span>
-                            <v-icon class="mr-1">mdi-share-variant</v-icon></v-row>
+                                <v-btn icon v-on:click="showComments(story)"><v-icon class="mr-1">mdi-comment</v-icon></v-btn>
+                                <span class="subheading mr-2">{{ story.comments.length }}</span>
+                                <span class="mr-1">·</span>
+                                <v-btn v-if="hasLiked(story.likes)" icon v-on:click="unlikeStory(story.id)">
+                                    <v-icon class="mr-1">mdi-heart</v-icon>
+                                </v-btn>
+                                <v-btn v-if="!hasLiked(story.likes)" icon v-on:click="likeStory(story.id)">
+                                    <v-icon class="mr-1">mdi-heart-outline</v-icon>
+                                </v-btn>
+                                <span class="subheading mr-2">{{ story.likes.length }}</span>
+                                <span class="mr-1">·</span>
+                                <v-icon class="mr-1">mdi-share-variant</v-icon>
+                            </v-row>
                         </v-list-item>
                     </v-card-actions>
                 </v-card>
             </v-row>
+
+            <v-dialog v-model="commentDialog" width="600px">
+                <v-card>
+                    <v-card-title>
+                     <span class="headline">Comments</span>
+                    </v-card-title>
+                    
+                    <v-card-text v-for="comment in commentDialogStory.comments" :key="comment.id">
+                        <v-list-item two-line>
+                            <v-list-item-content>
+                                <v-list-item-title><strong>{{ comment.user.name }}</strong> says:</v-list-item-title>
+                                <v-list-item-content>{{ comment.body }}</v-list-item-content>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-card-text>
+
+                    <v-card-text v-if="commentDialogStory.comments && commentDialogStory.comments.length == 0" align="center">
+                        No comments found
+                    </v-card-text>
+
+                    <v-card-text v-if="$store.state.authenticated" align="center">
+                        <v-text-field v-model="comment" label="Post a comment"></v-text-field>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green darken-1" :disabled="!comment" text @click="postComment(commentDialogStory.id)">Post a comment</v-btn>
+                        <v-btn color="green darken-1" text @click="closeComments()">Close</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+
+            <v-snackbar
+                v-model="snackbar"
+                :timeout="snackbarTimeout">
+                {{ snackbarText }}
+
+                <template v-slot:action="{ attrs }">
+                    <v-btn
+                        color="blue"
+                        text
+                        v-bind="attrs"
+                        @click="snackbar = false">
+                        Close
+                    </v-btn>
+                </template>
+            </v-snackbar>
         </v-container>
     </div>
 </template>
@@ -82,19 +138,94 @@
 <script>
 export default {
     data: () => ({
-        stories: []
+        stories: [],
+        commentDialog: false,
+        commentDialogStory: {},
+        comment: null,
+        snackbarTimeout: 2000,
+        snackbar: false,
+        snackbarText: ''
     }),
     mounted () {
-        this.$server.get('api/story').then((resp) => {
-            if (resp.data.status === 'SUCCESS') {
-                this.stories = resp.data.message.stories;
-            } else {
-                this.storeis = [];
-            }
-        });
+        this.fetchStories();
     },
     methods: {
-       
+        hasLiked(likes) {
+            let likedFlag = false;  
+            likes.forEach((value) => {
+                if (value.user_id == this.$store.state.user.id) {
+                    likedFlag = true;
+                }
+            });
+
+            return likedFlag;
+        },
+        fetchStories() {
+            this.$server.get('api/story').then((resp) => {
+                if (resp.data.status === 'SUCCESS') {
+                    this.stories = resp.data.message.stories;
+                } else {
+                    this.stories = [];
+                }
+            });
+        },
+        showComments(story) {
+            this.commentDialogStory = story;
+            this.commentDialog = true;
+        },
+        closeComments() {
+            this.commentDialogStory = {};
+            this.commentDialog = false;
+        },
+        postComment(storyId) {
+            if (this.comment != null || this.comment != '') {
+                this.$server.post('api/story/comment', {
+                    story_id: storyId,
+                    body: this.comment
+                }).then((resp) => {
+                    if (resp.data.status === 'SUCCESS') {
+                        this.fetchStories();
+                        this.commentDialogStory.comments.push({
+                            user: {
+                                name: this.$store.state.user.name
+                            },
+                            body: this.comment,
+                        });
+
+                        this.comment = null;
+                        this.snackbar = true;
+                        this.snackbarText = 'Comment posted';
+                    } else {
+                        this.snackbar = true;
+                        this.snackbarText = 'Comment post failed, please try again later';
+                    }
+                });
+            }
+        },
+        unlikeStory(storyId) {
+            this.$server.delete('api/story/unlike/' + storyId).then((resp) => {
+                if (resp.data.status === 'SUCCESS') {
+                    this.fetchStories();
+                    this.snackbar = true;
+                    this.snackbarText = 'You have unliked this story';
+                } else {
+                    this.snackbar = true;
+                    this.snackbarText = 'Can\'t unlike now, please try again later';
+                }
+            })
+        },
+        likeStory(storyId) {
+            this.$server.post('api/story/like/' + storyId).then((resp) => {
+                if (resp.data.status === 'SUCCESS') {
+                    this.fetchStories();
+                    this.snackbar = true;
+                    this.snackbarText = 'You have liked this story';
+                } else {
+                    this.snackbar = true;
+                    this.snackbarText = 'Can\'t like now, please try again later';
+                }
+            })
+        }
     }
 }
 </script>
